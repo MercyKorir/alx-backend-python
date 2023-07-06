8a66741355e497ffbc505f8003f859c74503c585
MercyKorir <mcthegreat78@gmail.com>
2023-07-06 08:23:01 +0300

    I have added the solution for task 2*

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Definition of a type-annotated function"""
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Takes in a floating number
+    returns the floor, float
+    """
+
+    return math.floor(n)
