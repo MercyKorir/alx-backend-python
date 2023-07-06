c4743ce541fedf55b2bda277146e1dc39a56380b
MercyKorir <mcthegreat78@gmail.com>
2023-07-06 08:28:37 +0300

    I have added the solution for task 3*

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Definition of type-annotated function"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float number and
+    returns str version of it
+    """
+
+    return str(n)
