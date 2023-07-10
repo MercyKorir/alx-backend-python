99f9f0d843957df209c32f491ffbaab03971840f
MercyKorir <mcthegreat78@gmail.com>
2023-07-10 20:41:07 +0300

    I have added the solution for the file 101*

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
 """definition of a type-annotated function"""
+import typing
+
+T = typing.TypeVar('T')
 
 
-def safely_get_value(dct, key, default = None):
+def safely_get_value(
+        dct: typing.Mapping,
+        key: typing.Any,
+        default: typing.Union[T, None] = None) -> typing.Union[typing.Any, T]:
+    """
+    takes in parameters and returns values
+    """
+
     if key in dict:
         return dct[key]
     else:
