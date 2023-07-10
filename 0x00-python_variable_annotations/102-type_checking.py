90a0bd6c14b2fedc769831ead64394ac38fbeff6
MercyKorir <mcthegreat78@gmail.com>
2023-07-10 20:20:33 +0300

    I have made corrections to the file 102-type*

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,22 +4,21 @@ import typing
 
 
 def zoom_array(
-        lst: typing.Union[typing.Tuple[typing.Any, ...],
-                          typing.List[typing.Any]],
+        lst: typing.Tuple[int, ...],
         factor: int = 2
-        ) -> typing.Tuple[typing.Any, ...]:
+        ) -> typing.List[int]:
     """
     Takes in a 2 args and returns a Tuple
     """
 
-    zoomed_in: typing.List[typing.Any] = [
+    zoomed_in: typing.List[int] = [
         item for item in lst
         for i in range(factor)
     ]
-    return tuple(zoomed_in)
+    return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
