d6a3a2248b3c7ecd7e1f97f8781465fd229b6a96
MercyKorir <mcthegreat78@gmail.com>
2023-08-15 12:29:13 +0300

    I have added the file for task 1*

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,15 @@ class TestAccessNestedMap(unittest.TestCase):
                                path: Tuple[Any, ...], expected_output: Any):
         """tests access_nested_map method"""
         self.assertEqual(access_nested_map(nested_map, path), expected_output)
+
+    @parameterized.expand([
+        ({}, ('a',)),
+        ({'a': 1}, ('a', 'b')),
+    ])
+    def test_access_nested_map_exception(self, nested_map: Dict[Any, Any],
+                                         path: Tuple[Any, ...]):
+        """tests for exception handling"""
+        with self.assertRaises(KeyError) as ex:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(ex.exception),
+                         "Key {} not found in {}".format(path[-1], nested_map))
