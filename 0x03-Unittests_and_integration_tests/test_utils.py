5853c649eaac55dbbd53e056955b28c67c03fab5
MercyKorir <mcthegreat78@gmail.com>
2023-08-15 13:06:12 +0300

    I have added the file for task 2*

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """Definition of class TestAccessNestedMap"""
 import unittest
+import unittest.mock
 from typing import Any, Dict, Tuple
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,3 +31,21 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(str(ex.exception),
                          "'{}'".format(path[-1]))
+
+
+class TestGetJson(unittest.TestCase):
+    """Inherits from TestCase"""
+
+    @unittest.mock.patch('requests.get')
+    def test_get_json(self, mock_get: unittest.mock.Mock) -> None:
+        """Test for get_json"""
+
+        test_cases = [
+            ('http://example.com', {'payload': True}),
+            ('http://holberton.io', {'payload': False})
+        ]
+        for test_url, test_payload in test_cases:
+            mock_get.return_value.json.return_value = test_payload
+            res = get_json(test_url)
+            mock_get.assert_called_with(test_url)
+            self.assertEqual(res, test_payload)
