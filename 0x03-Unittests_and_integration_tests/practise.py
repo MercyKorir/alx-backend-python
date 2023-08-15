6bd206027b30c9a5b6288451c2fdf4ab86a91ba0
MercyKorir <mcthegreat78@gmail.com>
2023-08-15 12:19:40 +0300

    I have added the file for task 0*

---
+++ 0x03-Unittests_and_integration_tests/practise.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+from utils import access_nested_map
+
+nested_map = {
+    'a': {
+        'b': {
+            'c': 1
+        }
+    },
+    'd': 2
+}
+
+valuec = access_nested_map(nested_map, ['a', 'b', 'c'])
+valued = access_nested_map(nested_map, ['d'])
+valueb = access_nested_map(nested_map, ['a', 'b'])
+valuea = access_nested_map(nested_map, ['a'])
+print("c: {}".format(valuec))
+print("d: {}".format(valued))
+print("b: {}".format(valueb))
+print("a: {}".format(valuea))
+
